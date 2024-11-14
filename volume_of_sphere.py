afc4dbedcc430507559dacae7efe5bb0f19194e0
shivalikattumadam <54284497+shivalikattumadam@users.noreply.github.com>
2024-11-14 17:57:30 -0600

    adding volume of sphere file

---
+++ volume_of_sphere.py
@@ -0,0 +1,13 @@
+def compute_volume_of_sphere(radius):
+	pi = 3.14
+	volume = (4/3) * pi * (radius ** 3)
+	return volume
+
+radius1 = 30
+area1 = compute_volume_of_sphere(radius1)
+print(f"The volume of sphere with radius {radius1} is: {area1}")
+
+radius2 = 40
+area2 = compute_volume_of_sphere(radius2)
+print(f"The volume of sphere with radius {radius2} is: {area2}")
+
